55c035afe8bb4e31af690e0f01e5a87c9e30e6f7
himbra <brahimanougmar19@gmail.com>
2024-07-15 20:33:14 +0100

    employee repository

---
+++ src/main/java/dev/himbra/employeemanagementsystem/employee/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package dev.himbra.employeemanagementsystem.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
+}
