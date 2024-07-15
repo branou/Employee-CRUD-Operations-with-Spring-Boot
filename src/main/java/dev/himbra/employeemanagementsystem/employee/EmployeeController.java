8667157009a7379dab4dee0e3869d3390571cebe
himbra <brahimanougmar19@gmail.com>
2024-07-15 20:37:51 +0100

    the controller that contains the http methods

---
+++ src/main/java/dev/himbra/employeemanagementsystem/employee/EmployeeController.java
@@ -0,0 +1,32 @@
+package dev.himbra.employeemanagementsystem.employee;
+
+import dev.himbra.employeemanagementsystem.base.PageResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employee")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+    @GetMapping("")
+    public ResponseEntity<PageResponse<Employee>> findAll(@RequestParam int page,@RequestParam int size){
+        return ResponseEntity.ok(employeeService.findAllEmployees(page,size));
+    }
+    @PostMapping("/save")
+    public ResponseEntity<Long> saveEmployee(@Valid @RequestBody EmployeeReq employee){
+        return ResponseEntity.ok(employeeService.createEmployee(employee));
+    }
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<Long> updateEmployee(@Valid @RequestBody EmployeeReq employeeReq,@PathVariable Long id){
+        return ResponseEntity.ok(employeeService.updateEmployee(id,employeeReq));
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id){
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
