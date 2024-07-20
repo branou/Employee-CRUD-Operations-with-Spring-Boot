631f1df107c0f51102f5b7eccbc434a2c1637609
himbra <brahimanougmar19@gmail.com>
2024-07-20 21:37:31 +0100

    change it again

--- src/main/java/dev/himbra/employeemanagementsystem/employee/EmployeeRepository.java
+++ src/main/java/dev/himbra/employeemanagementsystem/employee/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package dev.himbra.employeemanagementsystem.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
