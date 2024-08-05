8d5599c1b733031b12df897baf4d35fde0bc9032
himbra <brahimanougmar19@gmail.com>
2024-08-05 21:53:36 +0100

    the use of the ordinary getall instead of the paginedted one

--- EmployeeManagement-Front/src/app/services/employee.service.ts
+++ EmployeeManagement-Front/src/app/services/employee.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Employee} from "./models/Employee";
-import {Observable} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +8,7 @@ export class EmployeeService {
   url:string='http://localhost:8080/api/employee';
   constructor(private http:HttpClient) { }
   getEmployees(){
-    return this.http.get<Array<Employee>>(`${this.url}/?page=1&size=1`);
+    return this.http.get<Array<Employee>>(`${this.url}/all`);
   }
   deleteEmployee(id:number){
     return this.http.delete<any>(`${this.url}/delete/${id}`);
