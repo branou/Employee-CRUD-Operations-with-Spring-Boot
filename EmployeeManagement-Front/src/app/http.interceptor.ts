7bb5073e6289a18785c32e341f9282cdba93f105
himbra <brahimanougmar19@gmail.com>
2024-08-09 20:17:40 +0100

    creating the interceptor to handle the loading of the spinner we can use it later on for passing a header

---
+++ EmployeeManagement-Front/src/app/http.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import {inject} from "@angular/core";
+import {LoadingService} from "./services/loading.service";
+import {finalize} from "rxjs";
+
+export const httpInterceptor: HttpInterceptorFn = (req, next) => {
+    const spinnerService = inject(LoadingService); // Inject the SpinnerService
+
+    spinnerService.show(); // Show the spinner
+
+    return next(req).pipe(
+        finalize(() => {
+            spinnerService.hide(); // Hide the spinner when the request completes
+        })
+    );
+};
