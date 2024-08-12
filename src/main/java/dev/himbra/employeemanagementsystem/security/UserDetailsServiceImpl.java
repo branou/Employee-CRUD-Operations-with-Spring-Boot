ab3873c7b4ee5c2292ac462cf00d7155484fcf85
himbra <brahimanougmar19@gmail.com>
2024-08-12 17:07:20 +0100

    implementing the userDetailsService

--- src/main/java/dev/himbra/employeemanagementsystem/security/UserDetailsServiceImpl.java
+++ src/main/java/dev/himbra/employeemanagementsystem/security/UserDetailsServiceImpl.java
@@ -1,15 +1,20 @@
 package dev.himbra.employeemanagementsystem.security;
 
+import dev.himbra.employeemanagementsystem.admin.Admin;
+import dev.himbra.employeemanagementsystem.admin.AdminRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private AdminRepository adminRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return adminRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("username not Found!"));
     }
 }
