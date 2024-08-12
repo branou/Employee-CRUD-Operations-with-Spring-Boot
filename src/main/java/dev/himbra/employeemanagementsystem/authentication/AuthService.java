ddf3699367a127e79f0bb9f987af395cda9d46c1
himbra <brahimanougmar19@gmail.com>
2024-08-13 00:29:19 +0100

    authentication service that contains the logic for the authentication methods

---
+++ src/main/java/dev/himbra/employeemanagementsystem/authentication/AuthService.java
@@ -0,0 +1,41 @@
+package dev.himbra.employeemanagementsystem.authentication;
+
+import dev.himbra.employeemanagementsystem.admin.Admin;
+import dev.himbra.employeemanagementsystem.admin.AdminRepository;
+import dev.himbra.employeemanagementsystem.role.Role;
+import dev.himbra.employeemanagementsystem.role.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+    public String login(LoginRequest loginRequest){
+        Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),loginRequest.getPassword()));
+        String token=null;
+        try{
+        token=jwtService.generateToken((Admin)authentication.getPrincipal());}
+        catch (Exception e){e.printStackTrace();}
+        return token;
+    }
+    public void register(RegisterRequest registerRequest){
+        Role role = roleRepository.findByName("ADMIN").orElseThrow(() -> new IllegalStateException("ROLE user was not initiated"));
+        Admin admin =Admin.builder().firstname(registerRequest.getFirstname())
+                .lastname(registerRequest.getLastname()).email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .roles(List.of(role))
+                .build();
+        adminRepository.save(admin);
+    }
+}
