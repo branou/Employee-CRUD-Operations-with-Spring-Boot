e3b9eb8628de28903b5a4e48024c6115870042db
himbra <brahimanougmar19@gmail.com>
2024-08-16 23:29:36 +0100

    creating a guard to protect the routes

---
+++ EmployeeManagement-Front/src/app/guards/auth.guard.ts
@@ -0,0 +1,13 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {TokenService} from "../services/token.service";
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router=inject(Router);
+  const tokenService=inject(TokenService);
+  if(tokenService.isTokenNotValid()){
+    router.navigate(['signin']);
+    return false;
+  }
+  return true;
+};
