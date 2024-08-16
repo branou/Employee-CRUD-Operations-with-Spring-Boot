2ac01520330f1a3114967562880ef95c5770e191
himbra <brahimanougmar19@gmail.com>
2024-08-16 23:31:31 +0100

    create a service to handle the returned token from login

---
+++ EmployeeManagement-Front/src/app/services/token.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {JwtHelperService} from "@auth0/angular-jwt";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+  token!:string;
+  constructor() { }
+  isTokenValid() {
+    const token = this.token;
+    if (!token) {
+      return false;
+    }
+    // decode the token
+    const jwtHelper = new JwtHelperService();
+    // check expiry date
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+    if (isTokenExpired) {
+      localStorage.clear();
+      return false;
+    }
+    return true;
+  }
+
+  isTokenNotValid() {
+    return !this.isTokenValid();
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+    this.token=token
+  }
+}
